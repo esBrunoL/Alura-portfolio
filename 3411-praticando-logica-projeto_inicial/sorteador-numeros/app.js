9a46373abd5de0cfd8da093fb61f38cab79a8b0d
esBrunoL <brunolobo.14@hotmail.com>
2024-10-25 16:01:31 -0400

    novo commit

---
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -0,0 +1,6 @@
+sortear(){
+    let quantity = document.getElementById("quantidade").value;
+    let de = document.getElementById("de").value;
+    let ate = document.getElementById("ate").value;
+    alert(`Quantidade = ${quantidade}\nDe = ${de}\nAte = ${ate}`);
+}

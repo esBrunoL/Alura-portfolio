d820e22664276506dc6b159497c8b574e4ca2b2b
esBrunoL <brunolobo.14@hotmail.com>
2024-11-26 13:10:07 -0500

    Lesser corrections of alugame project

--- 3411-praticando-logica-projeto_inicial/alugames/js/app.js
+++ 3411-praticando-logica-projeto_inicial/alugames/js/app.js
@@ -1,15 +1,16 @@
 
 function alterarStatus(i){
-    var btn = document.getElementById(i);
-    
-    var img = document.getElementById("img-"+i);
+    let game= document.getElementById("game-"+i);
+    let img = game.querySelector('.dashboard__item__img');
+    let btn = game.querySelector('.dashboard__item__button');
+    let gameName = game.querySelector('.dashboard__item__name');
 
     if(btn.className.match("dashboard__item__button dashboard__item__button--return")){
         btn.className="dashboard__item__button";
         btn.textContent="Alugar";
 
         img.className = "dashboard__item__img"
-
+        alert(`Thank you for returning ${gameName.textContent}`);
         
 
     }else{
@@ -17,7 +18,7 @@ function alterarStatus(i){
         btn.textContent="Devolver";
 
         img.className ="dashboard__item__img dashboard__item__img--rented"
-
+        alert(`Enjoy ${gameName.textContent}`);
 
     }
 
